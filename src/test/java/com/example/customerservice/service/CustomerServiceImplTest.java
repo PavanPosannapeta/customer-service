02e8536607f4ccf22ba7fa991fbc60499f2c9c16
Pavan Posannapeta <pavan.posannapeta@gmail.com>
2023-01-03 22:15:43 +0530

    Pavan, Mani | Add unit test cases

---
+++ src/test/java/com/example/customerservice/service/CustomerServiceImplTest.java
@@ -0,0 +1,25 @@
+package com.example.customerservice.service;
+
+import com.example.customerservice.web.model.Customer;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerServiceImplTest {
+
+    @Test
+    void getAllCustomers() {
+        assertFalse(true);
+    }
+
+    @Test
+    void getCustomerById() {
+        CustomerService customerServiceImpl = new CustomerServiceImpl();
+        Customer customer =
+                customerServiceImpl.getCustomerById(UUID.fromString("7b4a4eaf-70a6-45f8-b589-7bdb320eb3ae"));
+        Assertions.assertEquals("Sachin", customer.getName());
+    }
+}
