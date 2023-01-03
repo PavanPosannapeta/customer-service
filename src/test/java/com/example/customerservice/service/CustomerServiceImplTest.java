ab01393816e84fd8a723db3faffe339f6aa4ba59
Pavan Posannapeta <pavan.posannapeta@gmail.com>
2023-01-03 22:18:54 +0530

    Pavan, Mani | Disable dummy test case

--- src/test/java/com/example/customerservice/service/CustomerServiceImplTest.java
+++ src/test/java/com/example/customerservice/service/CustomerServiceImplTest.java
@@ -2,6 +2,7 @@ package com.example.customerservice.service;
 
 import com.example.customerservice.web.model.Customer;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.util.UUID;
@@ -11,6 +12,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class CustomerServiceImplTest {
 
     @Test
+    @Disabled
     void getAllCustomers() {
         assertFalse(true);
     }
