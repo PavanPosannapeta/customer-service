848981f0a1d4595b0d8d7f9e28036c85847c0ebb
Pavan Posannapeta <pavan.posannapeta@gmail.com>
2023-03-16 19:32:35 +0530

    Updates cloud build configuration

--- terraform/artifact_registry.tf
+++ terraform/artifact_registry.tf
@@ -1,5 +1,5 @@
 resource "google_artifact_registry_repository" "my-repo" {
-  location      = "us-central1"
+  location      = "asia-south1"
   repository_id = "customer-service"
   format        = "DOCKER"
 
